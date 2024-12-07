ca93bf3ddb91f1abb736e1d779de4bf8dd160b4c
Zain Mahmoud <zain.mahmoud@outlook.com>
2024-12-06 20:43:26 -0500

    Fix basePath and assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {unoptimized: true}
+  output: 'export', // Ensure this is set for static export
+  images: {
+    unoptimized: true, // Disable image optimization for GitHub Pages
+  },
+  basePath: isProd ? '/ReactWebResume' : '', // Replace with your GitHub repo name
+  assetPrefix: isProd ? '/ReactWebResume/' : '', // Ensure assets load properly
 };
 
 export default nextConfig;
